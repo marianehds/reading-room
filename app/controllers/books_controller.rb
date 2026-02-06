dda19479e938e86abd3f05617350d041208d8439
Mariane <marianehds@outlook.com>
2026-02-06 13:05:29 -0300

    feat: implementa edição, visualização e exclusão de livros com modal

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,10 @@
 class BooksController < ApplicationController
-  before_action :require_login, only: [:new, :create]
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :require_login, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
 
   def index
-    @books = Book.includes(:user).all
+    @books = Book.includes(:user).order(created_at: :desc)
   end
 
   def new
@@ -13,21 +15,47 @@ class BooksController < ApplicationController
     @book = current_user.books.build(book_params)
 
     if @book.save
-      redirect_to books_path, notice: "Livro cadastrado!"
+      redirect_to root_path, notice: "Livro cadastrado!"
     else
       render :new, status: :unprocessable_entity
     end
   end
-  
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to root_path, notice: "Livro atualizado!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to root_path, notice: "Livro removido!"
+  end
+
   def search
     title = params[:title]
     results = OpenLibraryClient.new.search_by_title(title, limit: 10)
-  
     render json: { results: results }
   end
-  
 
   private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def authorize_user!
+    return if @book.user == current_user
+    redirect_to root_path, alert: "Você não tem permissão para isso"
+  end
 
   def book_params
     params.require(:book).permit(:title, :author, :published_year)
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  get "/favicon.ico", to: redirect("/icon.png")
+
   get "books/index"
   get "books/new"
   get "books/create"
   get "books/search", to: "books#search"
-  root "home#index"
+  root "books#index"
 
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
-  resources :books, only: [:index, :new, :create]
+  resources :books, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   
 
 end
