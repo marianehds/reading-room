f85295d5de8bfa32bf2a59072e223fd7a1aa906c
Mariane <marianehds@outlook.com>
2026-02-08 20:28:12 -0300

    feat: adiciona validação para o ano de publicação e requer campos de título e autor nos formulários de criação e edição de livros

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,10 @@ class Book < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :author, presence: true
+  validates :published_year, presence: true,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 1000,
+              less_than_or_equal_to: -> { Date.current.year }
+            }
 end
