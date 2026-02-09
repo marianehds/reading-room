9eb9745fa7ba85bb141de428b706dafe01d7b049
Mariane <marianehds@outlook.com>
2026-02-09 12:00:22 -0300

    refactor: simplifica rotas de livros, agrupando ações em recursos e mantendo a busca como uma coleção

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   get "/favicon.ico", to: redirect("/icon.png")
 
-  get "books/index"
-  get "books/new"
-  get "books/create"
-  get "books/search", to: "books#search"
   root "books#index"
 
-  resources :users, only: [:new, :create]
-  resource :session, only: [:new, :create, :destroy]
-  resources :books, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  
+  resources :books do
+    collection do
+      get :search
+    end
+  end
 
+  resources :users, only: [:new, :create]
+  resource  :session, only: [:new, :create, :destroy]
 end
