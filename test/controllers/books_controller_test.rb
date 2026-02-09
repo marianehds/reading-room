254902dc72873868f00d5c6c190cfa74176462f7
Mariane <marianehds@outlook.com>
2026-02-09 12:17:07 -0300

    feat: implementa teste para criação de livros, garantindo que apenas usuários autenticados possam adicionar novos livros

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -11,8 +11,26 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get create" do
-    get books_create_url
-    assert_response :success
+  test "should create book when logged in" do
+    user = User.create!(
+      name: "Ana",
+      email: "ana@example.com",
+      password: "secret123",
+      password_confirmation: "secret123"
+    )
+
+    post session_url, params: { email: user.email, password: "secret123" }
+
+    assert_difference("Book.count", 1) do
+      post books_url, params: {
+        book: {
+          title: "O Livro",
+          author: "A Autora",
+          published_year: 2020
+        }
+      }
+    end
+
+    assert_redirected_to root_path
   end
 end
